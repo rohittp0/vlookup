cfc53bd2fd2ddcef8ed743a8997f3cdd33d6b69b
Rohit T P <tprohit9@gmail.com>
2024-01-13 17:38:50 +0530

    move

--- server/home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from home import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('search/', views.search, name='search'),
-    path('organization/',views.organization, name='organization'),
-]
